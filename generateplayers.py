74f153b9ba3f74b27fc801ed0b1177461b8dde6a
derkiz <aconejulien@gmail.com>
2022-07-30 19:19:07 +0200

    removed list from player gen added names module

--- generateplayers.py
+++ generateplayers.py
@@ -1,129 +1,20 @@
 import random
+import names
 
 # Generate 10 NPCs onto a list.
 
-# Names to chose from
-names = [
-    'Russell Kirk',
-    'Peter Wheeler',
-    'Hans Moon',
-    'Manuela Skinner',
-    'Velma Mahoney',
-    'Carl Huynh',
-    'Kent Miller',
-    'Duane Best',
-    'Angela Pena',
-    'Josef Lucero',
-    'Coy Bruce',
-    'Margery Vaughan',
-    'Chad Mcintosh',
-    'Deon Francis',
-    'Juana Burch',
-    'Thelma Daniel',
-    'Christie Lowe',
-    'Deangelo Phelps',
-    'Glen Swanson',
-    'Antone Hayes',
-    'Elisabeth James',
-    'Carrol Pruitt',
-    'Errol Fischer',
-    'Noreen Guzman',
-    'Rene Mays',
-    'Yvette Rodriguez',
-    'Noelle Ramos',
-    'Nellie Morales',
-    'Wesley Daniels',
-    'Chang Montes',
-    'Sandra Dudley',
-    'Bert Pace',
-    'Paulette Clay',
-    'Leopoldo Galvan',
-    'Reed Mcdowell',
-    'Martina Donovan',
-    'Glenda Arias',
-    'Nadine Pearson',
-    'Colette Good',
-    'Mia Floyd',
-    'Millie Tran',
-    'Josefa Davenport',
-    'Pat Farley',
-    'Javon Herring',
-    'Ralph Higgins',
-    'Gillian Hendrix',
-    'Laura Levine',
-    'Eileen Chen',
-    'Agustin Huang',
-    'Kristin Pruitt',
-    'Liana Hubbard',
-    'Hillary Price',
-    'Myla Hubbard',
-    'Raelynn Good',
-    'Martin Gross',
-    'Rebecca Silva',
-    'Kade Carson',
-    'Vaughn Cain',
-    'Maggie Oconnor',
-    'Amanda Ray',
-    'Karissa Haas',
-    'Aron Mccoy',
-    'Norah Nash',
-    'Mina Perkins',
-    'Karter Santana',
-    'Savion Brandt',
-    'Aleah Shea',
-    'Trevor Taylor',
-    'Steve Salinas',
-    'Sidney Li',
-    'Madilyn Spencer',
-    'Kasen Garza',
-    'Sandra Wall',
-    'Kaleb Brennan',
-    'Luz Everett',
-    'Savannah Wiley',
-    'Haven Malone',
-    'Antwan Howe',
-    'Jazmin Gay',
-    'Mohamed Porter',
-    'Isiah Fox',
-    'Geovanni Meadows',
-    'Brody Bentley',
-    'Kayley Parsons',
-    'Branden Carroll',
-    'Kassandra Shaffer',
-    'Avah Mitchell',
-    'Ashly Travis',
-    'Kamari Beck',
-    'Jeramiah Acevedo',
-    'Lucian Hanna',
-    'Baylee Bernard',
-    'Aliya Walker',
-    'Maren Flowers',
-    'Jase Whitehead',
-    'Annika Craig',
-    'Jacey Keller',
-    'Mckayla Horne',
-    'Kasey Glass',
-    'Colby Ellison',
-    'Lorena Petty',
-]
+npc_list = []
 
-# Empty List for Generation Function
-gen_list = []
+def gen_list():
 
+    hp = 100
 
-# Random Names into a compiled list
-# To change length of list modify the 10 to any positive whole number in while statement
-def ran_gen(lists):
-    x = 0
-    while x != 10:
-        x += 1
-        n = (random.randint(0, 99))
-        if (names[n]) in gen_list:
-            x -= 1
+    while len(npc_list) < 10:
+        random_int = random.randint(1, 10)
+        if random_int < 8:
+            minus_hp_ran = 0
         else:
-            gen_list.append(names[n])
+            minus_hp_ran = random.randint(1, 55)
+        npc_list.append((names.get_full_name(), hp - minus_hp_ran))
 
-
-ran_gen(1)
-
-# To check use: print(gen_list)
+gen_list()
